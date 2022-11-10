4768dde1fbb34d0c3b6df849eceeec3dbb7d2598
Francisco Fonseca <francisco.fonseca997@gmail.com>
2022-11-10 01:57:43 -0600

    project push

--- App.js
+++ App.js
@@ -1,11 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { Keyboard, ScrollView, StyleSheet, Text, View } from "react-native";
+import ListInput from "./components/ListInput";
+import ListItem from "./components/ListItem";
 
 export default function App() {
+  const [tasks, setTasks] = useState([]);
+
+  const addTask = (task) => {
+    if (task == null) return;
+    setTasks([...tasks, task]);
+    Keyboard.dismiss();
+  };
+
+  const deleteTask = (deleteIndex) => {
+    setTasks(tasks.filter((value, index) => index != deleteIndex));
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.heading}>TODO LIST</Text>
+      <ScrollView style={styles.scrollView}>
+        {tasks.map((task, index) => {
+          return (
+            <View key={index} style={styles.taskContainer}>
+              <ListItem
+                index={index + 1}
+                task={task}
+                deleteTask={() => deleteTask(index)}
+              />
+            </View>
+          );
+        })}
+      </ScrollView>
+      <ListInput addTask={addTask} />
     </View>
   );
 }
@@ -13,8 +40,20 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#1E1A3C",
+  },
+  heading: {
+    color: "#fff",
+    fontSize: 20,
+    fontWeight: "600",
+    marginTop: 30,
+    marginBottom: 10,
+    marginLeft: 20,
+  },
+  scrollView: {
+    marginBottom: 70,
+  },
+  taskContainer: {
+    marginTop: 20,
   },
 });
